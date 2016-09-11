68aaedbedc1d46023681500ba42423f9926e7a4b
gorifla <gorifla@gmail.com>
2016-09-11 11:34:06 -0300

    Feita class para formatar o CEP

---
+++ src/Cep.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Valida e formata o Cep
+ *
+ * @link      http://github.com/realejo/library
+ * @copyright Copyright (c) 2014 Realejo Design Ltda. (http://www.realejo.com.br)
+ */
+namespace Realejo;
+
+class Cep
+{
+    /**
+     * Formata o CEP no padrão 00000-000
+     * Não é feita nenhuma validação
+     *
+     * @param string $cep CEP com ou sem formatação
+     *
+     * @return string
+     */
+    public static function format($cep)
+    {
+        // Reduz ao CEP desformatado
+        $cep = self::unformat($cep);
+
+        // Verifica se há um CEP
+        if (!empty($cep)) {
+            $cep = substr($cep, 0, 5) . '-' . substr($cep, 5, 3);
+        }
+
+        // Retorna o CEP formatado
+        return $cep;
+    }
+
+    /**
+     * Remove a formatação do CEP, reduzindo a apenas numeros
+     *
+     * @param string $cep
+     * @return string
+     */
+    public static function unformat($cep)
+    {
+        // Remove tudo que não for numeros
+        $cep = preg_replace('/[^0-9]/', '', $cep);
+
+        // Verifica se sobrou numero para o CEP
+        //@todo verificar o tamamho minimo de um CEP
+        if (!empty($cep)) {
+            return str_pad($cep, 8, '0', STR_PAD_LEFT);
+        }
+
+        // Se não achou um CEP possível retorna vazio
+        return '';
+    }
+}
---
+++ test/CepTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace RealejoTest;
+
+/**
+ * CEP test case.
+ */
+use Realejo\Cep;
+
+class CepTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests Cep::unformat()
+     */
+    public function testUnformat()
+    {
+        $this->assertStringMatchesFormat('', Cep::unformat(null));
+        $this->assertStringMatchesFormat('', Cep::unformat(''));
+        $this->assertStringMatchesFormat('', Cep::unformat('string'));
+
+        $this->assertEquals(8, strlen(Cep::unformat(1)));
+
+        $this->assertStringMatchesFormat('00000001', Cep::unformat(1));
+        $this->assertStringMatchesFormat('00000001', Cep::unformat('1'));
+        $this->assertStringMatchesFormat('00000012', Cep::unformat('12'));
+        $this->assertStringMatchesFormat('00000123', Cep::unformat('123'));
+        $this->assertStringMatchesFormat('00001234', Cep::unformat('1234'));
+        $this->assertStringMatchesFormat('00012345', Cep::unformat('12345'));
+        $this->assertStringMatchesFormat('00123456', Cep::unformat('123456'));
+        $this->assertStringMatchesFormat('01234567', Cep::unformat('1234567'));
+        $this->assertStringMatchesFormat('12345678', Cep::unformat('12345678'));
+        $this->assertStringMatchesFormat('23456789', Cep::unformat('23456789'));
+        $this->assertStringMatchesFormat('34567890', Cep::unformat('34567890'));
+        $this->assertStringMatchesFormat('45678901', Cep::unformat('45678901'));
+        $this->assertStringMatchesFormat('45678901', Cep::unformat(45678901));
+    }
+
+    /**
+     * Tests Cep::format()
+     */
+    public function testFormat()
+    {
+        $this->assertEquals('', Cep::format(null));
+        $this->assertEquals('', Cep::format(''));
+        $this->assertEquals('', Cep::format('string'));
+
+        $this->assertEquals(9, strlen(Cep::format(1)));
+
+        $this->assertStringMatchesFormat('00000-001', Cep::format(1));
+        $this->assertStringMatchesFormat('00000-001', Cep::format('1'));
+        $this->assertStringMatchesFormat('00000-012', Cep::format('12'));
+        $this->assertStringMatchesFormat('00000-123', Cep::format('123'));
+        $this->assertStringMatchesFormat('00001-234', Cep::format('1234'));
+        $this->assertStringMatchesFormat('00012-345', Cep::format('12345'));
+        $this->assertStringMatchesFormat('00123-456', Cep::format('123456'));
+        $this->assertStringMatchesFormat('01234-567', Cep::format('1234567'));
+        $this->assertStringMatchesFormat('12345-678', Cep::format('12345678'));
+        $this->assertStringMatchesFormat('23456-789', Cep::format('23456789'));
+        $this->assertStringMatchesFormat('34567-890', Cep::format('34567890'));
+        $this->assertStringMatchesFormat('45678-901', Cep::format('45678901'));
+        $this->assertStringMatchesFormat('45678-901', Cep::format(45678901));
+
+        $this->assertStringMatchesFormat('00000-001', Cep::format('00000001'));
+        $this->assertStringMatchesFormat('45678-901', Cep::format('45678901'));
+        $this->assertStringMatchesFormat('00000-001', Cep::format('00000-001'));
+        $this->assertStringMatchesFormat('45678-901', Cep::format('45678-901'));
+    }
+
+}
