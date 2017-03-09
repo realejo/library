f918c092e9f8890638282e9d418a891e75293067
Rodrigo Capile <rcapile@gmail.com>
2017-03-09 16:40:51 -0300

    função para retornar o tamanho em Kb, Mb, etc

--- src/File.php
+++ src/File.php
@@ -10,6 +10,26 @@ namespace Realejo;
 class File
 {
     /**
+     * @param $bytes
+     * @param int $precision
+     * @return string
+     */
+    public static function formatBytes($bytes, $precision = 2)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        // Uncomment one of the following alternatives
+        $bytes /= pow(1024, $pow);
+        // $bytes /= (1 << (10 * $pow));
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+
+    /**
      * Lê um arquivo e retorna o contéudo em pedaços (chunk)
      * Deve ser usado quando tiver um download com acesso restrito e/ou
      * o arquivo for muito grande ou puder ser lido em pedaços. Ex: FLV, MPG
